fe39d43263468bd3abe55fcfb37d216f37bc16c9
somiahgit <somiahjamal1997@gmail.com>
2020-09-11 18:33:19 +0300

    dataClass

---
+++ src/ClassDataclass.kt
@@ -0,0 +1,25 @@
+class Players1 { 
+    data class PlayersData(val playerName: String, val TeamName: String, val playerNum: Int)
+
+    fun task(): List<PlayersData> {
+        return listOf(PlayersData("Hassan", "Barcelona", 9),
+                      PlayersData("Mohammed", "Real Madrid", 7))
+    }
+}
+    ////////////////////////////////////////////////////////////////////////
+class Players2(val playerName: String, val TeamName: String, val playerNum: Int){
+
+        fun task(): List<Players2> {
+            return listOf(Players2("Hassan", "Barcelona", 9),
+                          Players2("Mohammed", "Real Madrid", 7))
+        }
+    }
+
+/*
+* data class for data and setter and getter just not fun
+* normal class for data and fun
+*
+* data class can not be defult constructor
+* normal class can be defult constructor
+*
+*/
